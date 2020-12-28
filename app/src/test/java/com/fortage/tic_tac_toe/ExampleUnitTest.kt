40ecbc6f378814fa5a0fd450eb7bff7ff823ccdc
Fortage <32445750+Fortage@users.noreply.github.com>
2020-12-28 10:18:46 +0500

    end
    end

---
+++ app/src/test/java/com/fortage/tic_tac_toe/ExampleUnitTest.kt
@@ -0,0 +1,12 @@
+package com.fortage.tic_tac_toe
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
